750aca6f0f29d41410459486fe5ee528856e3e8c
guan xin <guanxin@tinytiger.cn>
2020-08-25 17:28:17 +0800

    解决错误

--- app/Controllers/Http/Api/LessonController.js
+++ app/Controllers/Http/Api/LessonController.js
@@ -2,11 +2,12 @@
 const Lesson = use('App/Models/Lesson')
 
 class LessonController {
-  index ({ request }) {
+  async index ({ request }) {
     const page = request.input('page', 1)
     const limit = request.input('limit', 20)
     const type = request.input('type', 1)
-    return await Lesson.query().where({status: 0, type: type}).paginate(page, limit)
+    let lessons = await Lesson.query().where({status: 0, type: type}).paginate(page, limit)
+    return lessons
   }
 }
 
