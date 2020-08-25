ad9bd758b05ca51a9b72aa25ea2cc21650a08a44
guan xin <guanxin@tinytiger.cn>
2020-08-25 17:57:10 +0800

    优化视频播放列表

--- app/Controllers/Http/Api/VideoController.js
+++ app/Controllers/Http/Api/VideoController.js
@@ -5,7 +5,12 @@ class VideoController {
   async index ({ request }) {
     const page = request.input('page', 1)
     const limit = request.input('limit', 20)
-    return await Video.query().where({status: 0}).paginate(page, limit)
+    const lesson_id = request.input('lesson_id', '')
+    let filters = {status: 0}
+    if(lesson_id !== '') {
+      filters.lesson_id = lesson_id
+    }
+    return await Video.query().where(filters).paginate(page, limit)
   }
 
   async show ({ params }) {
