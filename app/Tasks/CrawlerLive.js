5a56780409d96623b1c47aa6300e141e2fb2d134
guan xin <guanxin@tinytiger.cn>
2020-09-21 18:21:52 +0800

    添加定时任务

--- app/Tasks/CrawlerLive.js
+++ app/Tasks/CrawlerLive.js
@@ -1,18 +1,24 @@
 'use strict'
 
+const Task = use('Task')
 const Http = use('got')
 const Video = use('App/Models/Video')
 const BaseUrl = 'https://hi.doniai.com/api/v1.0'
 
-class TestController {
-  async index () {
-    let tasks = await this.test()
+class CrawlerLive extends Task {
+  static get schedule () {
+    return '1 */3 * * * *'
+  }
+
+  async handle () {
+    this.info('Task CrawlerLive handle')
+    let tasks = await this.getCrawlerTasks()
     tasks.forEach(async task => {
       await this.doTask(task)
     });
   }
 
-  async test() {
+  async getCrawlerTasks() {
     const tasks = await Video.query().where({type: 1, is_crawler: 1}).fetch()
     return tasks.toJSON()
   }
@@ -22,17 +28,10 @@ class TestController {
     let return_data = JSON.parse(body)
     let live_url = return_data.data.live_url
     let video_id = task.id
-
     if(return_data.code === 200) {
-      console.log(task.id)
-      console.log('ada');
-      await Video
-      .query()
-      .where('id', task.id)
-      .update({ url: live_url })
+      await Video.query().where('id', video_id).update({ url: live_url })
     }
-    return body
   }
 }
 
-module.exports = TestController
+module.exports = CrawlerLive
