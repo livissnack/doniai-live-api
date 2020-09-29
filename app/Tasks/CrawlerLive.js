dda1f4c56c35489f678deb692681f919f1700769
guan xin <guanxin@tinytiger.cn>
2020-09-29 17:52:35 +0800

    修改douyu抓源

--- app/Tasks/CrawlerLive.js
+++ app/Tasks/CrawlerLive.js
@@ -4,6 +4,7 @@ const Task = use('Task')
 const Http = use('got')
 const Video = use('App/Models/Video')
 const BaseUrl = 'https://hi.doniai.com/api/v1.0'
+const TinyTigerUrl = 'http://114.55.166.223/getliveurl'
 
 class CrawlerLive extends Task {
   static get schedule () {
@@ -24,12 +25,30 @@ class CrawlerLive extends Task {
   }
 
   async doTask(task) {
-    const { body } = await Http.get(`${BaseUrl}/live/${task.platform}`, {searchParams: {roomid: task.roomid}});
-    let return_data = JSON.parse(body)
-    let live_url = return_data.data.live_url
+    const live_url = await this.getLiveUrl(task)
     let video_id = task.id
     if(return_data.code === 200) {
       await Video.query().where('id', video_id).update({ url: live_url })
+    }
+  }
+
+  async getLiveUrl(task) {
+    if(task.platform === 'douyu') {
+      const { body } = await Http.get(`${TinyTigerUrl}`, {searchParams: {url: `https://www.douyu.com/${task.roomid}`}});
+      let return_data = JSON.parse(body)
+      if(return_data.status === 'true') {
+        let live_url = return_data['hls'][0]['data'][0]['url'][0]
+        return live_url.replace(/^http:\/\//i, 'https://')
+      } else {
+        return ''
+      }
+    }
+
+    if(task.platform === 'huya') {
+      const { body } = await Http.get(`${BaseUrl}/live/${task.platform}`, {searchParams: {roomid: task.roomid}});
+      let return_data = JSON.parse(body)
+      let live_url = return_data.data.live_url
+      return live_url
     }
   }
 }
